0320078b5fbe9fa2ec66ba701665bc0ea35741f3
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-28 17:23:52 +0200

    Created currencies array in separated file

---
+++ src/currencies.js
@@ -0,0 +1,25 @@
+const currencies = [
+    {
+        id: "eur",
+        name: "Euro",
+        code: "EUR",
+        symbol: "€",
+        exchangeRate: 4.4600
+    },
+    {
+        id: "usd",
+        name: "Dolar amerykański",
+        code: "USD",
+        symbol: "$",
+        exchangeRate: 4.0719
+    },
+    {
+        id: "gbp",
+        name: "Funt szterling",
+        code: "GBP",
+        symbol: "£",
+        exchangeRate: 5.1764
+    },
+];
+
+export { currencies };
