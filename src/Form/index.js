2dec99cc6efb72a9cee78edd1a0ecace81cda7d8
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-28 17:30:42 +0200

    Formatted Form component for better readability

--- src/Form/index.js
+++ src/Form/index.js
@@ -9,43 +9,42 @@ const Form = () => {
         event.preventDefault();
     };
 
-    return (<form
-        className="form"
-        onSubmit={onFormSubmit}
-    >
-        <legend>
-            Kwota w złotówkach:
-        </legend>
-
-        <input
-            className="form__input"
-            type="number"
-            value={inputValue}
-            onChange={({ target }) => { setInputValue(target.value) }}
-            min={0}
-            placeholder="Wpisz kwotę"
-            autoFocus
-            required
-        />
-
-        <legend>
-            Waluta:
-        </legend>
-
-        <select className="form__select">
-            {currencies.map(currency => (
-                <option key={currency.id}>
-                    {currency.name}
-                </option>
-            ))}
-        </select>
-
-        <button
-            className="form__button"
+    return (
+        <form
+            className="form"
+            onSubmit={onFormSubmit}
         >
-            Przelicz kwotę
-        </button>
-    </form>
+            <legend>
+                Kwota w złotówkach:
+            </legend>
+
+            <input
+                className="form__input"
+                type="number"
+                value={inputValue}
+                onChange={({ target }) => { setInputValue(target.value) }}
+                min="0"
+                placeholder="Wpisz kwotę"
+                autoFocus
+                required
+            />
+
+            <legend>
+                Waluta:
+            </legend>
+
+            <select className="form__select">
+                {currencies.map(currency => (
+                    <option key={currency.id}>
+                        {currency.name}
+                    </option>
+                ))}
+            </select>
+
+            <button className="form__button">
+                Przelicz kwotę
+            </button>
+        </form>
     );
 };
 
