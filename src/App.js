628d0502aeec901ab508473286cf3edc81782a8c
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-29 12:49:43 +0200

    Imported useState, currencies, created functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Container from "./Container";
 import Header from "./Header";
 import Form from "./Form";
 import Result from "./Result";
+import { currencies } from "./currencies";
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
+
+  const handleCurrencyChange = ({ target }) => {
+    const selectedCurrencyId = target.value;
+    const selected = currencies.find((currency) => currency.id === selectedCurrencyId);
+    setSelectedCurrency(selected);
+  };
+
   return (
     <Container>
       <Header title="Kalkulator Walut" />
-      <Form />
+      <Form
+        inputValue={inputValue}
+        handleCurrencyChange={handleCurrencyChange}
+        setInputValue={setInputValue}
+      />
       <Result />
     </Container>
   );
