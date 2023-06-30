53b3bc26350dc30ed54f2466d24ee108b8aa189d
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-06-30 22:59:58 +0200

    Removed unused "symbol" properties from currencies

--- src/currencies.js
+++ src/currencies.js
@@ -2,19 +2,16 @@ const currencies = [
     {
         id: "Euro",
         code: "EUR",
-        symbol: "€",
         exchangeRate: 4.4600
     },
     {
         id: "Dolar amerykański",
         code: "USD",
-        symbol: "$",
         exchangeRate: 4.0719
     },
     {
         id: "Funt szterling",
         code: "GBP",
-        symbol: "£",
         exchangeRate: 5.1764
     },
 ];
