59bbcc20baf884dabea520e8aaf5a86256036d0a
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-07-02 21:38:31 +0200

    Removed unnecessary <div>

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,11 +1,9 @@
 import "./style.css";
 
 const Result = ({ refreshedInputValue, convertedValue, refreshedSelectedCurrency }) => (
-    <div>
         <p className="result">
             {refreshedInputValue} PLN = <span className="result__boldText">{convertedValue.toFixed(2)} {refreshedSelectedCurrency}</span>
         </p>
-    </div>
 );
 
 export default Result;
